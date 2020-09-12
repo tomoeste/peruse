e3747e0e91193f21c9644b82d4a8af4b5427d17e
Tom Oeste <tom.oeste@bullseye-computing.com>
2020-09-12 12:03:28 -0400

    Updated icon

--- app/main.dev.ts
+++ app/main.dev.ts
@@ -60,6 +60,7 @@ const createWindow = async () => {
     show: false,
     width: 1024,
     height: 728,
+    icon: '/resources/icon.png',
     webPreferences:
       (process.env.NODE_ENV === 'development' ||
         process.env.E2E_BUILD === 'true') &&
